3c72d4a8e43a36c88ed4feea574269388cd362cb
Dluxxx <104112814+Dluxxx@users.noreply.github.com>
2023-12-02 16:44:48 +0700

    Update StrukturKelas.jsx

--- src/Pages/StrukturKelas.jsx
+++ src/Pages/StrukturKelas.jsx
@@ -108,22 +108,20 @@ const StrukturKelas = () => {
             />
           </div>
           <div
-					className=" absolute bottom-[14.5%] lg:hidden"
-					data-aos="fade-up"
-					data-aos-duration="1200">
-					<div className="flex justify-center items-center flex-col">
-						<img src="LineVertikal3.svg" alt="" className="lg:hidden" />
-						<img src="Circle.svg" alt="" className="relative top-[-0.7rem]" />
-						<div className="relative bottom-3">
-							<BorderStruktur Jabatan="Kesehatan" Nama="haha" Width="120px" />
-							<div className="py-[3%]"></div>
-							<BorderStruktur Jabatan="" Nama="Haha" Width="120px" />
-						</div>
-					</div>
-				</div>
-        </div>
+            className=" absolute bottom-[14.5%] lg:hidden"
+	    data-aos="fade-up"
+	    data-aos-duration="1200">
+	    <div className="flex justify-center items-center flex-col">
+	    <img src="LineVertikal3.svg" alt="" className="lg:hidden" />
+	    <img src="Circle.svg" alt="" className="relative top-[-0.7rem]" />
+	    <div className="relative bottom-3">
+	    <BorderStruktur Jabatan="Sekretaris" Nama="Mozelle" Width="120px" />
+	    <div className="py-[3%]"></div>
+	    </div>
+	</div>
       </div>
-    </div>
+   </div>
+  </div>
   );
 };
 
