eee8bbaac5608b13f0ba724172f3bccaea6082af
Dluxxx <104112814+Dluxxx@users.noreply.github.com>
2023-12-04 06:43:46 +0700

    Update Senin.jsx

--- src/components/Mapel/Senin.jsx
+++ src/components/Mapel/Senin.jsx
@@ -24,7 +24,7 @@ const Senin = () => {
         </div>
       </div>
       <div
-        className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
+        className=" border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
         data-aos="fade-up"
         data-aos-duration="700"
       >
