1e86ba6a1adcb26401ead5f0e036d094af64bb1a
Dluxxx <104112814+Dluxxx@users.noreply.github.com>
2024-01-12 21:16:15 +0700

    Update Jumat.jsx

--- src/components/Mapel/Jumat.jsx
+++ src/components/Mapel/Jumat.jsx
@@ -81,12 +81,12 @@ const Senin = () => {
         <div className="flex justify-center items-center text-sm">
           13.00-14.00
         </div>
-        <div
+      </div>
+      <div
         className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
         data-aos="fade-up"
         data-aos-duration="1000"
       ></div>
-      </div>
     </>
   );
 };
