3e688fe80e10009f5ab15cf3fb71fafa1abf5205
Dluxxx <104112814+Dluxxx@users.noreply.github.com>
2024-01-12 21:04:26 +0700

    Update Jumat.jsx

--- src/components/Mapel/Jumat.jsx
+++ src/components/Mapel/Jumat.jsx
@@ -82,11 +82,6 @@ const Senin = () => {
           13.00-14.00
         </div>
       </div>
-      <div
-        className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
-        data-aos="fade-up"
-        data-aos-duration="1000"
-      ></div>
     </>
   );
 };
