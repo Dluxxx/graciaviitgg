86937d48aa5bc69bd1d52f837fac92bb52621239
Dluxxx <104112814+Dluxxx@users.noreply.github.com>
2025-12-01 21:33:58 +0700

    Update Kamis.jsx

--- src/components/Mapel/Kamis.jsx
+++ src/components/Mapel/Kamis.jsx
@@ -6,7 +6,7 @@ const Senin = () => {
         data-aos="fade-up"
         data-aos-duration="600"
       >
-        <div className="w-[50%] text-base  font-medium">Math</div>
+        <div className="w-[50%] text-base  font-medium">IPA</div>
         <div className="flex justify-center items-center text-sm">
           07.00-08.00
         </div>
@@ -16,7 +16,7 @@ const Senin = () => {
         data-aos="fade-up"
         data-aos-duration="600"
       >
-        <div className="w-[50%] text-base  font-medium">Chemistry</div>
+        <div className="w-[50%] text-base  font-medium">Prakarya</div>
         <div className="flex justify-center items-center text-sm">
           08.00-09.00
         </div>
@@ -36,7 +36,7 @@ const Senin = () => {
         data-aos="fade-up"
         data-aos-duration="800"
       >
-        <div className="w-[50%] text-base  font-medium">Olahraga</div>
+        <div className="w-[50%] text-base  font-medium">Bahasa Indonesia</div>
         <div className="flex justify-center items-center text-sm">
           09.15-10.15
         </div>
@@ -47,7 +47,7 @@ const Senin = () => {
         data-aos="fade-up"
         data-aos-duration="900"
       >
-        <div className="w-[50%] text-base  font-medium">Agama</div>
+        <div className="w-[50%] text-base  font-medium">P5</div>
         <div className="flex justify-center items-center text-sm">
           10.15-11.15
         </div>
@@ -67,7 +67,7 @@ const Senin = () => {
         data-aos="fade-up"
         data-aos-duration="1000"
       >
-        <div className="w-[50%] text-base font-medium">English</div>
+        <div className="w-[50%] text-base font-medium">Matematika</div>
         <div className="flex justify-center items-center text-sm">
           12.00-13.00
         </div>
@@ -77,7 +77,7 @@ const Senin = () => {
         data-aos="fade-up"
         data-aos-duration="1000"
       >
-        <div className="w-[50%] text-base font-medium">Math</div>
+        <div className="w-[50%] text-base font-medium">IPA</div>
         <div className="flex justify-center items-center text-sm">
           13.00-14.00
         </div>
@@ -87,7 +87,7 @@ const Senin = () => {
         data-aos="fade-up"
         data-aos-duration="1000"
       >
-        <div className="w-[50%] text-base font-medium">Matematika</div>
+        <div className="w-[50%] text-base font-medium">IPS</div>
         <div className="flex justify-center items-center text-sm">
           14.00-15.00
         </div>
@@ -97,3 +97,4 @@ const Senin = () => {
 };
 
 export default Senin;
+
