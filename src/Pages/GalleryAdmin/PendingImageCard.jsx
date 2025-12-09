9c9d92b57d86f6d55b086638d7c55cad6d376b80
Dluxxx <104112814+Dluxxx@users.noreply.github.com>
2025-12-09 22:46:58 +0700

    Update PendingImageCard.jsx

--- src/Pages/GalleryAdmin/PendingImageCard.jsx
+++ src/Pages/GalleryAdmin/PendingImageCard.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { supabase } from "../../supabase/supabase";
+import { supabase } from "../../supabase";
 
 export default function PendingImageCard({ file, onDone }) {
 
